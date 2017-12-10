f798b70f835fa6c638104f5c92ca14f24a1b5cd3
John Shao <shaojianyong@hotmail.com>
2017-12-10 13:53:17 +0800

    sematic-angular-template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  toggle(): void {
+    console.log('Hello World!');
+    $('.ui.sidebar').sidebar({
+      context: 'body',
+      dimPage: false
+      /*
+      onVisible: function() {
+          $('body').click(function(e){
+              this.unbind(e);
+          });
+      },
+      onShow: function() {
+          $('.ui.sidebar').css("z-index",999);
+      },
+      onHide: function() {
+          $('.ui.sidebar').css("z-index",1);
+      }*/
+    }).sidebar('toggle');
+  }
+
 }
